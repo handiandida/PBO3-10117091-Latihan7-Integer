f286ae2e6953de97bbcabf80f598a1f5737113e5
Dida Handian <handiandida@gmail.com>
2018-09-23 19:45:37 +0700

    Integer

---
+++ src/pbo3/pkg10117091/latihan7/integer/PBO310117091Latihan7Integer.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo3.pkg10117091.latihan7.integer;
+
+/* @author
+* NAMA  : DIDA HANDIAN
+* KELAS : PBO3
+* NIM   : 10117091
+* Deskripsi Program : Program ini berisi contoh penulisan dengan type data 
+                      Integer
+*/
+public class PBO310117091Latihan7Integer {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        byte b;
+        short s;
+        int i;
+        long l;
+        b = 120;
+        s = 32767;
+        i = 1_000_000_000;
+        l = 90000000000L;
+        System.out.println("byte : " + b );
+        System.out.println("short : " + s );
+        System.out.println("int : " + i );
+        System.out.println("long : " + l );
+    }
+    
+}
